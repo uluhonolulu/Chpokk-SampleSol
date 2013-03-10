b86ae0c6292317380c66d52997065475d4ac334b
Artem Smirnov <uluhonolulu@gmail.com>
2013-03-10 22:04:56 +0200

    Commit yo!

--- src/ConsoleApplication1/Class2.cs
+++ src/ConsoleApplication1/Class2.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace ConsoleApplication1 {
-	public class Class2 {
-		public void Do2() {}
-	}
+namespace ConsoleApplication1 {
+    public class Class2 {
+        public void Do2() {
+//yo man
+    }
+    }
 }
+
+
